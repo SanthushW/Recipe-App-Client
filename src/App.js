4510dee189a683c3d45872e66b7de2bbd65d35e8
nimansu-fernando <nimansufernando@gmail.com>
2024-12-09 16:53:15 +0530

    NC21GD-31 Add Recipe of the Week section with styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomeSearchBar from "./components/HomeSearchBar";
 import EffortlessEats from "./components/EffortlessEats";
 import HealthyEatingInspiration from "./components/HealthyEatingInspiration";
 import LiquidHarmony from "./components/LiquidHarmony";
+import RecipeOfTheWeek from "./components/RecipeOfTheWeek";
 import SetYourPreferences from "./components/SetYourPreferences";
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <EffortlessEats />
       <HealthyEatingInspiration />
       <LiquidHarmony />
+      <RecipeOfTheWeek />
       <SetYourPreferences />
     </div>
   );
---
+++ src/components/RecipeOfTheWeek.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../styles/RecipeOfTheWeek.css";
+
+const RecipeOfTheWeek = () => {
+  return (
+    <div className="recipe-of-the-week">
+      <h2 className="section-title">Recipe of the Week</h2>
+      <div className="recipe-content">
+        <div className="recipe-image-container">
+          <img
+            src="butter-garlic-chicken.jpg"
+            alt="Butter Garlic Chicken"
+            className="recipe-image"
+          />
+          <div className="play-button">
+            <span className="play-icon">&#9654;</span>
+          </div>
+        </div>
+        <div className="recipe-details">
+          <h3 className="recipe-title">Butter Garlic Chicken</h3>
+          <p className="recipe-author">
+            By <span className="author-name">GARIMA KAPUR</span>
+          </p>
+          <p className="recipe-time">Total time: 20 mins</p>
+          <p className="recipe-description">
+            This easy garlic butter chicken recipe is a weeknight fave! Make
+            perfect chicken breast with butter, garlic, and herbs, in just 20
+            minutes.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeOfTheWeek;
